fdd0fd8826606f74c625334784315d030fe702e4
Dias21B030874 <98642870+Dias21B030874@users.noreply.github.com>
2023-02-22 00:13:09 +0600

    Lab4

---
+++ Lab4/Lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import {Product, products} from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(p: Product) {
+    const url = encodeURI(p.prodLink);
+    const telegram = `https://t.me/share/url?url=${url}`;
+    const whatsapp = `https://api.whatsapp.com/send?text=${url}`;
+    window.open(whatsapp);
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
