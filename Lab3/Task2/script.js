c220a92bb8282cc65146cf1050d04c58d6b731ae
Dias21B030874 <98642870+Dias21B030874@users.noreply.github.com>
2023-03-08 23:14:06 +0600

    лаба 6

--- Lab3/Task2/script.js
+++ Lab3/Task2/script.js
@@ -1,6 +1,8 @@
 const addButton = document.querySelector('.add_button')
 const taskList = document.querySelector('.tasks')
 const newTask = document.querySelector('.new_task')
+const data = document.getElementById('lol')
+const datanow = document.getElementById('lol1')
 
 addButton.addEventListener('click', function () {
     const addTask = newTask.value.trim();
@@ -9,7 +11,11 @@ addButton.addEventListener('click', function () {
         return;
     const taskText = document.createElement('span');
     taskText.innerHTML = addTask;
-
+    let somer = data.value;
+    let some = document.createElement('div')
+    some.innerHTML=somer
+    let today = datanow.value;
+    
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.addEventListener('click', crossOut);
@@ -22,19 +28,32 @@ addButton.addEventListener('click', function () {
 
     const item = document.createElement('div');
 
+    if(today < somer){
+        item.style.color = 'red'
+    }
+
     item.className = 'task-item'
     item.appendChild(checkbox)
     item.appendChild(taskText);
+    item.appendChild(some)
     item.appendChild(removeTask);
 
     taskList.appendChild(item);
 });
 
+// function crossOutDeadline(){
+//     var today = new Date(now.getDay(), now.getMonth(), now.getFullYear())
+//     if(today - 86400000 > somer)
+//         this.parentNode.className.toggle("deadline")
+     
+// }
+
 function crossOut() {
+
     if (this.checked)
-        this.parentNode.className = 'task-item done';
+        this.parentNode.className = 'task-item done' ;
     else
-        this.parentNode.className = 'task-item undone';
+         this.parentNode.className = 'task-item undone';
 }
 
 function deleteItem() {
