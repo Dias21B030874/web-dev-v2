999c326ca5b9df8f2257a7ee8fa5c87cca24d73f
Dias21B030874 <98642870+Dias21B030874@users.noreply.github.com>
2023-03-26 13:38:54 +0600

    defense

--- Lab6/CRUD-example/src/app/albums.service.ts
+++ Lab6/CRUD-example/src/app/albums.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Album} from './template';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,20 @@ export class AlbumsService {
   BASE_URL: string = 'https://jsonplaceholder.typicode.com';
 
   constructor(private client: HttpClient) { }
-
+  
   getPosts(): Observable<Album[]> {
     return this.client.get<Album[]>(`${this.BASE_URL}/albums`)
+    .pipe(
+      map(albums => {
+        return albums.map(album => {
+          return {
+            id: album.id,
+            title: album.title.toUpperCase(),
+            userId: album.userId
+          };
+        });
+      })
+    )
   }
 
   getPost(id: number): Observable<Album> {
@@ -20,11 +33,12 @@ export class AlbumsService {
   }
 
   addPost(album: Album): Observable<Album> {
-    return this.client.post<Album>(`${this.BASE_URL}/albums`, album);
+    return this.client.post<Album>(`${this.BASE_URL}/albums`, album)
+       
   }
 
   updatePost(id: number, title: string): Observable<Album> {
-    return this.client.patch<Album>(`${this.BASE_URL}/albums/${id}`, title);
+    return this.client.patch<Album>(`${this.BASE_URL}/albums/${id}`, title)
   }
 
   deletePost(id: number): Observable<any> {
--- Lab6/CRUD-example/src/app/app.component.ts
+++ Lab6/CRUD-example/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  
 }
